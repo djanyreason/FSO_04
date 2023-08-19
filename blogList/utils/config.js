7c816811b0a7f5f404531153dd6fba759e9067f2
djanyreason <djanyreason@gmail.com>
2023-08-19 12:00:10 -0400

    Adds libraries and global teardwon
    This commit adds dependencies to the json configuration files for the
    cross-env, supertest, and express-async-errors libraries.
    
    For cross-env, it also modifies npm run commands in package.json to
    define the environment, and modifies utils/config.js to set the DB
    URL depending on whether or not the process was started in a 'test'
    environment.
    
    For express-async-errors, the commit also modifies app.js to require
    the library.
    
    This commit also adds tests/teardown.js as a global teardown for
    jest, and defines it as such in package.json.

--- blogList/utils/config.js
+++ blogList/utils/config.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3003;
-const BLOGDB_URI = process.env.BLOGLISTDB_URI;
+const BLOGDB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_BLOGLISTDB_URI
+  : process.env.BLOGLISTDB_URI;
 
 module.exports = {
   BLOGDB_URI,
