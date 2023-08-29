fec25b0e995c3055e38a70160a7b03431f6356cb
djanyreason <djanyreason@gmail.com>
2023-08-29 11:40:59 -0400

    Completes ex. 4.15, adds users collection to DB
    This commit adds functionality for a users collection to the bloglist
    DB. The User model is defined in models/user.js, and contains
    username, name, and passwordHash fields. Access to the users
    collection is managed in controllers/users.js, and currently consists
    of POST to create new users and GET to get the entire list of users.
    The GET returns data with name, username, and ID formatted as a
    string.
    
    The commit also updates app.js to use the functions in
    controllers/users.js.
    
    The commit uses bcrypt to perform the password hashing - it is added
    as a dependency in the package json files.

--- blogList/app.js
+++ blogList/app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 const app = express();
 const cors = require('cors');
 const blogListRouter = require('./controllers/blogs');
+const userRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 const mongoose = require('mongoose');
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/blogs', blogListRouter);
+app.use('/api/users', userRouter);
 
 app.use(middleware.errorHandler);
 
